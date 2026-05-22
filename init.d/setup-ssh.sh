3ee6a56015cbbdca499ae128a50ff66d524c087c
Matthias Meyer <matthias.meyer@sdsc.ethz.ch>
2026-05-22 14:15:05 +0200

    Update setup-ssh.sh

--- init.d/setup-ssh.sh
+++ init.d/setup-ssh.sh
@@ -150,5 +150,7 @@ fi
 
 # Set PI_CODING_AGENT_DIR so it persists in sessions
 echo "export PI_CODING_AGENT_DIR=/home/renku/work/.pi/agent" >> ~/.bashrc
+echo "export CLAUDE_CONFIG_DIR=/home/renku/work/.claude" >> ~/.bashrc
+echo "export CODEX_HOME=/home/renku/work/.codex" >> ~/.bashrc
 echo "export BROWSER=true" >> ~/.bashrc
 # PI_CODING_AGENT_DIR=/home/renku/work/.pi/agent pi install npm:pi-sandbox
